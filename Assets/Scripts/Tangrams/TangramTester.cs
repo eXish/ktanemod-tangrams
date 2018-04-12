289fe8566fe31fc6dcee7c3a8932db383baed24a
Ashley Muncaster <ashley@atbweb.co.uk>
2018-04-12 23:00:09 +0100

    Add missing connection on PL-04.

---
+++ Assets/Scripts/Tangrams/TangramTester.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TangramTester : MonoBehaviour
+{
+    public TangramGrid Grid;
+    public TangramPiece[] Pieces;
+
+    private KMBombModule _bombModule = null;
+
+    private void Awake()
+    {
+        _bombModule = gameObject.AddComponent<KMBombModule>();
+
+        Grid.Load();
+        foreach (TangramPiece piece in Pieces)
+        {
+            piece.Load();
+        }
+
+        Tangram tangram = new Tangram(Grid, Pieces);
+        tangram.LogInfo(_bombModule);
+    }
+}
