1585d1809051ed16696943968be6defa65ecc6a9
Ashley Muncaster <ashley@atbweb.co.uk>
2018-06-30 14:02:45 +0100

    Change how LitText works.
    "Fixes" issue when a gameplay room fires off no lighting events (i.e. always presumed on if no lighting events get fired).
    (v1.0.2 mod config).

--- Assets/Scripts/LitText.cs
+++ Assets/Scripts/LitText.cs
@@ -5,39 +5,23 @@ public class LitText : MonoBehaviour
     public TextMesh Text = null;
     private KMGameInfo _gameInfo = null;
 
-    private static bool _lightState = false;
+    private bool _lightState = true;
 
     private void Awake()
     {
         _gameInfo = GetComponentInParent<KMGameInfo>();
         _gameInfo.OnLightsChange += OnLightsChange;
-        _gameInfo.OnStateChange += OnStateChange;
         OnLightsChange(_lightState);
     }
 
     private void OnDestroy()
     {
         _gameInfo.OnLightsChange -= OnLightsChange;
-        _gameInfo.OnStateChange -= OnStateChange;
     }
 
     private void OnLightsChange(bool on)
     {
         Text.GetComponent<MeshRenderer>().enabled = on;
         _lightState = on;
-    }
-
-
-    private void OnStateChange(KMGameInfo.State state)
-    {
-        switch (state)
-        {
-            case KMGameInfo.State.Gameplay:
-                break;
-
-            default:
-                _lightState = false;
-                break;
-        }
     }
 }
